97aff792b276f7739aac41ff82a163a103611801
lukebeer <mail@luke.beer>
2015-09-20 15:26:19 +0100

    SoftwarePipeline renamed to PathPipeline and truncated storage path for documentation

--- broadsoftxchange/pipelines.py
+++ broadsoftxchange/pipelines.py
@@ -15,11 +15,14 @@ except ImportError:
 from scrapy.http import Request
 
 
-class SoftwarePipeline(FilesPipeline):
+class PathPipeline(FilesPipeline):
     def file_path(self, request, response=None, info=None):
         if not isinstance(request, Request):
             url = request
         else:
             url = request.url
         bits = urlparse(url)
-        return bits.netloc + bits.path
+        path = bits.netloc + bits.path
+        if path.startswith('xchange.broadsoft.com/php/xchange/system/files/'):
+            return path.strip('xchange.broadsoft.com/php/xchange/system/files/')
+        return path
