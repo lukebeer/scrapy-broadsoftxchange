fe3f57f1e1d0a7c918b3f19e0d6d4262aa961f51
lukebeer <mail@luke.beer>
2015-09-20 15:25:02 +0100

    Added file_urls & files field to DocumentItem

--- broadsoftxchange/items.py
+++ broadsoftxchange/items.py
@@ -4,7 +4,8 @@ import scrapy
 
 class DocumentItem(scrapy.Item):
     title = scrapy.Field()
-    file = scrapy.Field()
+    file_urls = scrapy.Field()
+    files = scrapy.Field()
     release = scrapy.Field()
     category = scrapy.Field()
     date = scrapy.Field()
